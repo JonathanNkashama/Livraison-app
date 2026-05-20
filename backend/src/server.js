34904bb88905855fb14503ab9e13f19be28f2f73
JonathanNkashama <livraison.app.rdc@gmail.com>
2026-05-20 09:00:31 +0100

    Fix CORS for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,11 @@ const server = http.createServer(app);
 const socketIO = require('./socket/index');
 socketIO.init(server);
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
